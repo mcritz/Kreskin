69cbcb595a2445693250b2fd64b4e99536d1ea17
Michael Critz <github@michaelcritz.com>
2018-01-15 13:41:40 -0800

    Added basic, stupid prediction model

---
+++ Sources/App/Models/Prediction.swift
@@ -0,0 +1,76 @@
+//
+//  Prediction.swift
+//  KreskinPackageDescription
+//
+//  Created by Michael Critz on 1/14/18.
+//
+
+import Vapor
+import FluentProvider
+import HTTP
+
+final class Prediction: Model {
+    let storage = Storage()
+    
+    var title: String
+    var description: String
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+    
+    init(row: Row) throws {
+        title = try row.get("title")
+        description = try row.get("description")
+    }
+    
+    func makeRow() throws -> Row {
+        var row = Row()
+        try row.set("title", title)
+        try row.set("description", description)
+        return row
+    }
+}
+
+extension Prediction: Preparation {
+    static func prepare(_ database: Database) throws {
+        try database.create(self) { builder in
+            builder.id()
+            builder.string("title")
+            builder.string("descriotion")
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
+
+extension Prediction: JSONConvertible {
+    convenience init(json: JSON) throws {
+        try self.init(
+            title: json.get("title"),
+            description: json.get("description")
+        )
+    }
+    func makeJSON() throws -> JSON {
+        var json = JSON()
+        try json.set("id", id)
+        try json.set("title", title)
+        try json.set("descriotion", description)
+        return json
+    }
+}
+
+extension Prediction: ResponseRepresentable { }
+
+
+
+
+
+
+
+
+
+
