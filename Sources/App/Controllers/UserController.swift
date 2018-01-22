46e2831f29fb33257e2e98535d8ea50c243cf860
Michael Critz <github@michaelcritz.com>
2018-01-21 17:37:10 -0800

    Added validation for User

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,21 @@
+//
+//  UserController.swift
+//  App
+//
+//  Created by Michael Critz on 1/21/18.
+//
+
+import Foundation
+
+class UserController {
+    func isValid(email testString: String?) -> Bool {
+        guard let realString: String = testString else { return false }
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: realString)
+    }
+    func isValid(name testString: String?) -> Bool {
+        guard let realString: String = testString else { return false }
+        return realString.count >= 3 ? true : false
+    }
+}
