9f669e3936428af77cd80d1c2290d2da946908b7
Michael Critz <github@michaelcritz.com>
2018-01-28 07:53:06 -0800

    refactored user

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,8 +6,39 @@
 //
 
 import Foundation
+import Vapor
 
-class UserController {
+final class UserController {
+    
+    let predixController = PredictionController()
+    
+    func index(_ req: Request) throws -> ResponseRepresentable {
+        var users = try User.makeQuery().all()
+        users = users.map{ user in
+            user.email = ""
+            user.password = nil
+            return user
+        }
+        let usersJSON = try users.makeJSON()
+        return usersJSON
+    }
+    
+    func predictionsForUser(with req: Request) throws -> ResponseRepresentable {
+        let maybeId = req.parameters["id"]?.int
+        guard let idx: Int = maybeId else {
+            throw Abort(.badRequest)
+        }
+        guard let user = try User.find(idx) else {
+            throw Abort(.notFound)
+        }
+        guard let predix = try predixController.preditions(user: user) else {
+            throw Abort(.internalServerError)
+        }
+        var responseJSON = JSON()
+        try responseJSON.set("predictions", predix)
+        return responseJSON
+    }
+    
     func isValid(email testString: String?) -> Bool {
         guard let realString: String = testString else { return false }
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
