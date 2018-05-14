fe89ab90bed0cf7fe9bada0a61853e136d26083e
Michael Critz <github@michaelcritz.com>
2018-05-13 18:49:53 -0700

    updated package

--- Package.swift
+++ Package.swift
@@ -1,24 +1,25 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Kreskin",
-    targets: [
-        Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"]),
-    ],
+//    exclude: [
+//        "Config",
+//        "Database",
+//        "Localization",
+//        "Public",
+//        "Resources",
+//    ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2, minor: 1),
-        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.2"),
+        //        .package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.4"),
+        .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0-rc.2.3"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "1.0.0-rc.2")
     ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
+    targets: [
+        .target(name: "App", dependencies: ["Leaf", "Auth", "PostgreSQL", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
     ]
 )
 
